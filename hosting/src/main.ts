99aed68a0b6b74410cc08b6a6b781d6f567cdc1f
khangplayscode <88513698+khang-07@users.noreply.github.com>
2025-03-23 00:33:51 -0500

    um where are the files...

---
+++ hosting/src/main.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCWIrf4zv0XgrXcyYDl-lPW3PFv5SNiERg",
+
+  authDomain: "khang-computer.firebaseapp.com",
+
+  projectId: "khang-computer",
+
+  storageBucket: "khang-computer.firebasestorage.app",
+
+  messagingSenderId: "410096108754",
+
+  appId: "1:410096108754:web:9dcb485b5961ced2d6d90b"
+
+};
+
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
