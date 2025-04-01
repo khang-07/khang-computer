0eb1f8b56986ebc0725eb8f4a11c7a6ce084b0f3
khangplayscode <88513698+khang-07@users.noreply.github.com>
2025-03-31 20:52:48 -0500

    until june comes

---
+++ hosting/src/login.ts
@@ -0,0 +1,16 @@
+const input_thing = document.getElementById("login");
+console.log(input_thing?.textContent);
+
+// SHFIT TAB = tab to the left
+// source: https://stackoverflow.com/questions/1052772/is-there-a-keyboard-shortcut-to-untab-move-a-block-of-code-to-the-left-in-ec
+
+// ^ the og function
+// let password = function() {
+//   let userInput = document.getElementById("login").value;
+//   console.log(userInput);
+//   if (userInput == process.env.FRIENDS_PASSWORD) {
+//     document.getElementById("herro").textContent = "yes!"
+//   } else {
+//     document.getElementById("herro").textContent = "nope... rip"
+//   }
+// }
